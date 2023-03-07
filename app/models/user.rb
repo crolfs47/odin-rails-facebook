19d1a7216fd154ce9931639fc7d99034305e6b94
Cathy Rolfs <crolfs47@gmail.com>
2023-03-07 11:00:31 -0600

    Set up model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  has_many :friendships
-  has_many :friends, through :friendships
+  has_many :posts
+  has_many :comments
+  has_many :likes
+  has_many :friendships # to get the friendships you requested
+  has_many :friends, through: :friendships
+  has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id # to get the friendships someone else sent to you
+  has_many :received_friends, through: :received_friendships, source: :user
 end
