b31bff7addd658c85375eca88b5333441de04c27
Cathy Rolfs <crolfs47@gmail.com>
2023-03-07 10:41:36 -0600

    Configure postgreSQL, create and migrate User model

---
+++ db/migrate/20230307163903_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :first_name
+      t.string :last_name
+      t.string :location
+      t.string :college
+      t.string :high_school
+      t.string :occupation
+      t.string :birthday
+
+      t.timestamps
+    end
+  end
+end
