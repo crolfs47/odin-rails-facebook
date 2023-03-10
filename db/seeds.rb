d1232710767bb2f2a6d79656303b9f1ed17129f8
Cathy Rolfs <crolfs47@gmail.com>
2023-03-10 12:57:41 -0600

    Install rspec-rails gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+  { username: 'crolfs', email: 'test@test.com', first_name: 'Cathy', last_name: 'Rolfs', location: 'Chicago' },
+  { username: 'alex', email: 'test1@test.com', first_name: 'Alex', last_name: 'Thornburg', location: 'Chicago' },
+  { username: 'guest', email: 'test2@test.com', first_name: 'Guest', last_name: 'Guest', location: 'Chicago' },
+  { username: 'crolfs', email: 'test@test.com', first_name: 'Cathy', last_name: 'Rolfs', location: 'Chicago' },
+])
