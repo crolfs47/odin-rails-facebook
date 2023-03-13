39d2779dc4fd76b90cc699930aa34c46af59532f
Cathy Rolfs <crolfs47@gmail.com>
2023-03-13 16:27:59 -0500

    Add additional user feature testing

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
   factory :user do
-    email { "test@test.com" }
-    password  { "password" }
-    full_name { 'Test' }
-    location { 'Chicago'}
+    trait :user1 do
+      email { "test@test.com" }
+      password  { "password" }
+      full_name { 'Test' }
+      location { 'Chicago'}
+    end
+
+    trait :user2 do
+      email { "test2@test.com" }
+      password  { "password" }
+      full_name { 'Test 2' }
+      location { 'Chicago'}
+    end
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
-  let!(:user) { FactoryBot.create(:user)}
+  let!(:user1) { FactoryBot.create(:user, :user1)}
+  let!(:user2) { FactoryBot.create(:user, :user2)}
 
   context 'When user is not logged in' do
     it 'requires login' do
@@ -12,21 +13,35 @@ RSpec.describe 'User', type: :feature do
 
   context 'When user is logged in' do
     it 'logs the user in' do
-      login_as(user)
-      visit user_path(id: user.id)
-      expect(page).to have_content(user.full_name)
+      login_as(user1)
+      visit user_path(id: user1.id)
+      expect(page).to have_content(user1.full_name)
     end
   end
 
+  context 'When viewing another users show page' do
+    it 'shows the users basic info' do
+      login_as(user1)
+      visit user_path(id: user2.id)
+      expect(page).to have_content(user2.full_name)
+      expect(page).to have_content(user2.location)
+    end
+  end
 
-  context 'When updating a user' do
+  context 'When updating user information' do
     it 'updates the user location' do
-      login_as(user)
-      visit edit_user_path(id: user.id)
+      login_as(user1)
+      visit edit_user_path(id: user1.id)
       fill_in 'Location', with: 'New York City'
       click_on 'Update User'
-      visit user_path(id: user.id)
+      visit user_path(id: user1.id)
       expect(page).to have_content('New York City')
+    end
+
+    it 'can only update current users info' do
+      login_as(user1)
+      visit user_path(id: user2.id)
+      expect(page).not_to have_content('Update')
     end
   end
 end
