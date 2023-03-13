cbe889fe6b3263aa6e9e0d97ba763c56ac8a4cd7
Cathy Rolfs <crolfs47@gmail.com>
2023-03-13 16:02:25 -0500

    Add user tests for logging in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,6 +3,22 @@ require 'rails_helper'
 RSpec.describe 'User', type: :feature do
   let!(:user) { FactoryBot.create(:user)}
 
+  context 'When user is not logged in' do
+    it 'requires login' do
+      visit root_path
+      expect(page).to have_content('Log in')
+    end
+  end
+
+  context 'When user is logged in' do
+    it 'logs the user in' do
+      login_as(user)
+      visit user_path(id: user.id)
+      expect(page).to have_content(user.full_name)
+    end
+  end
+
+
   context 'When updating a user' do
     it 'updates the user location' do
       login_as(user)
@@ -11,7 +27,6 @@ RSpec.describe 'User', type: :feature do
       click_on 'Update User'
       visit user_path(id: user.id)
       expect(page).to have_content('New York City')
-      user.destroy!
     end
   end
 end
