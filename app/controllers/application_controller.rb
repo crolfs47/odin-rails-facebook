20bcc8c2e6a0b87007e06e2d7a7a076710b51637
Cathy Rolfs <crolfs47@gmail.com>
2023-03-13 15:53:53 -0500

    Add devise controllers, fix devise sign up page for full_name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_sign_up_params, if: :devise_controller?
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
   end
+
 end
