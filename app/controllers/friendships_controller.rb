bdf6063fc80de5be1cf63f24902986c8c28ec74c
Cathy Rolfs <crolfs47@gmail.com>
2023-03-13 16:46:34 -0500

    Add params to friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,11 @@
 class FriendshipsController < ApplicationController
+  def create
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id)
+  end
+
 end
