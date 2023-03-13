37c3472015bc42780192071fad7dc73e9c7c2a95
Cathy Rolfs <crolfs47@gmail.com>
2023-03-13 10:20:45 -0500

    Add devise gem, posts controller, edit user model to remove username and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
   
   has_many :posts
   has_many :comments
