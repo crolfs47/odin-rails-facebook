04c02d70776ad20243d2f1718e45110b2c592eb5
Cathy Rolfs <crolfs47@gmail.com>
2023-03-14 14:18:32 -0500

    Update user model associations to break out friends into pending vs accepted, update friendships index page for new associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,16 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships, dependent: :destroy # to get the friendships you requested
-  has_many :friends, through: :friendships, dependent: :destroy
-  has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id, dependent: :destroy # to get the friendships someone else sent to you
-  has_many :received_friends, through: :received_friendships, source: :user, dependent: :destroy
+
+  # To get friendships current user requested
+  has_many :friendships, dependent: :destroy 
+  # has_many :friends, through: :friendships, dependent: :destroy
+  has_many :accepted_friends_sent, -> { merge(Friendship.accepted) }, through: :friendships, source: :friend
+  has_many :pending_friends_sent, -> { merge(Friendship.pending) }, through: :friendships, source: :friend
+
+  # To get friendships someone else sent to current user
+  has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id, dependent: :destroy 
+  # has_many :received_friends, through: :received_friendships, source: :user, dependent: :destroy
+  has_many :accepted_friends_rec, -> { merge(Friendship.accepted) }, through: :received_friendships, source: :user
+  has_many :pending_friends_rec, -> { merge(Friendship.pending) }, through: :received_friendships, source: :user
 end
