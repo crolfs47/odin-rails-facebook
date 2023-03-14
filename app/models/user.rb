05c1db90e2ee7cb3cbee1e79548b7d4e476cd0e8
Cathy Rolfs <crolfs47@gmail.com>
2023-03-14 14:26:23 -0500

    Add method to get all friends to user model, update friendships index page to display all friend requests and status

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,17 @@ class User < ApplicationRecord
 
   # To get friendships current user requested
   has_many :friendships, dependent: :destroy 
-  # has_many :friends, through: :friendships, dependent: :destroy
   has_many :accepted_friends_sent, -> { merge(Friendship.accepted) }, through: :friendships, source: :friend
   has_many :pending_friends_sent, -> { merge(Friendship.pending) }, through: :friendships, source: :friend
 
   # To get friendships someone else sent to current user
   has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id, dependent: :destroy 
-  # has_many :received_friends, through: :received_friendships, source: :user, dependent: :destroy
   has_many :accepted_friends_rec, -> { merge(Friendship.accepted) }, through: :received_friendships, source: :user
   has_many :pending_friends_rec, -> { merge(Friendship.pending) }, through: :received_friendships, source: :user
+
+  def friends
+    accepted_friends_sent + accepted_friends_rec
+  end
+
+
 end
