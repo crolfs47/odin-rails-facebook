fdaa6d4963686df4e4f6f2930bb8db10fa07681e
Cathy Rolfs <crolfs47@gmail.com>
2023-03-16 15:58:07 -0500

    Add scopes for notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
     @user = current_user
+    @notifications = Notification.where(user: current_user)
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,9 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifiable, polymorphic: true
+
+  scope :unread, -> { where(read: nil) }
+  scope :friendships, -> { where(action: 'friendship') }
+  scope :likes, -> { where(action: 'like') }
+  scope :comments, -> { where(action: 'comment') }
 end
