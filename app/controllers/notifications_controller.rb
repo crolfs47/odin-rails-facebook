d9c3ac5ddf5a594d6c6874ac5d0e2d781ba98b39
Cathy Rolfs <crolfs47@gmail.com>
2023-03-16 16:32:49 -0500

    Set up notifications index to show unread/read notices for friendship notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    @user = current_user
     @notifications = Notification.where(user: current_user)
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,7 @@ class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read: nil) }
+  scope :read, -> { where(read: true) }
   scope :friendships, -> { where(action: 'friendship') }
   scope :likes, -> { where(action: 'like') }
   scope :comments, -> { where(action: 'comment') }
