1424840f7ccfff6c52883eaf09217e0b260d0aab
Cathy Rolfs <crolfs47@gmail.com>
2023-03-20 11:45:41 -0500

    Add notification dropdown in navbar, add unread_count variable, fix friendship mdoel so that notifications get destroyed

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,11 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = Notification.where(user: current_user)
+    @unread_count = @notifications.unread.length
   end
 
   def mark_as_read
+    # add if statement for friendship/like/comment instead of having it in views
     @notification = Notification.find(params[:id])
     @notification.update(read: true)
     redirect_to user_friendships_path(current_user)
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
-  has_many :notifications, as: :notifiable
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   scope :accepted, -> { where(accepted: true)}
   scope :pending, -> { where(accepted: false)}
