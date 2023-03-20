21a1c209f65657957cf92ed32330886f5b7d0e5c
Cathy Rolfs <crolfs47@gmail.com>
2023-03-20 15:46:43 -0500

    Set up posts controller and index view to create and show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
   end
   
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    redirect_to root_path
+    if @post.save
+      flash[:notice] = "New post created."
+    else
+      flash[:warning] = "Error: #{@post.errors.full_messages.join}"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :content)
   end
 end
