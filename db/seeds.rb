fdddc67139eb11f69540d0e01a943c3bbf5d0419
Cathy Rolfs <crolfs47@gmail.com>
2023-03-30 11:10:28 -0500

    Update seeds to create more users, update friends view if there are no friends or no pending friends

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,24 @@ Notification.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('notifications')
 
 users = User.create([
-  { email: 'crolfs47@gmail.com', password: 'password', full_name: 'Cathy Rolfs', location: 'Chicago' },
-  { email: 'test1@test.com', password: 'password', full_name: 'Alex Thornburg', location: 'Chicago' },
-  { email: 'test2@test.com', password: 'password', full_name: 'Guest', location: 'Chicago' },
-  { email: 'test3@test.com', password: 'password', full_name: 'Pete Davidson', location: 'Chicago' },
+  { email: 'crolfs47@gmail.com', password: 'password', full_name: 'Cathy Rolfs', location: 'Chicago, IL', birthday: "1989-06-01", college: "Notre Dame", high_school: "Chicago High School", occupation: "Student" },
+  { email: 'monstera@test.com', password: 'password', full_name: 'Monstera Deliciosa', location: 'Chicago, IL', birthday: "2000-01-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'pothos@test.com', password: 'password', full_name: 'Golden Pothos', location: 'Chicago, IL', birthday: "2000-02-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'snakeplant@test.com', password: 'password', full_name: 'Snake Plant', location: 'Chicago, IL', birthday: "2000-03-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'birdofparadise@test.com', password: 'password', full_name: 'Bird Of Paradise', location: 'Chicago, IL', birthday: "2000-04-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'jadeplant@test.com', password: 'password', full_name: 'Jade Plant', location: 'Chicago, IL', birthday: "2000-05-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'fiddleleaffig@test.com', password: 'password', full_name: 'Fiddle Leaf Fig', location: 'Chicago, IL', birthday: "2000-06-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'dracaena@test.com', password: 'password', full_name: 'Dracaena Marginata', location: 'Chicago, IL', birthday: "2000-07-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'pricklypear@test.com', password: 'password', full_name: 'Prickly Pear', location: 'Chicago, IL', birthday: "2012-08-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'rubberplant@test.com', password: 'password', full_name: 'Rubber Plant', location: 'Chicago, IL', birthday: "2012-08-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'burrostail@test.com', password: 'password', full_name: 'Burros Tail', location: 'Chicago, IL', birthday: "2012-10-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'topsyturvy@test.com', password: 'password', full_name: 'Topsy Turvy', location: 'Chicago, IL', birthday: "2012-11-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'echeveria@test.com', password: 'password', full_name: 'Echeveria Elegans', location: 'Chicago, IL', birthday: "2012-12-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'parlorpalm@test.com', password: 'password', full_name: 'Parlor Palm', location: 'Chicago, IL', birthday: "2015-02-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'ponytailpalm@test.com', password: 'password', full_name: 'Ponytail Palm', location: 'Chicago, IL', birthday: "2015-03-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'aloevera@test.com', password: 'password', full_name: 'Aloe Vera', location: 'Chicago, IL', birthday: "2015-04-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'pilea@test.com', password: 'password', full_name: 'Pilea Peperomioides', location: 'Chicago, IL', birthday: "2015-05-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'prayerplant@test.com', password: 'password', full_name: 'Prayer Plant', location: 'Chicago, IL', birthday: "2015-06-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'pincushioncactus@test.com', password: 'password', full_name: 'Pincushion Cactus', location: 'Chicago, IL', birthday: "2015-07-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
+  { email: 'alocasiaodora@test.com', password: 'password', full_name: 'Alocasia Odora', location: 'Chicago, IL', birthday: "2015-08-01", college: "Plant University", high_school: "Plant High School", occupation: "Plant" },
 ])
