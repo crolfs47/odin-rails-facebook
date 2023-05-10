3a8644b9bbf081f7043a2835a5c32f61ce5b13ab
Cathy Rolfs <crolfs47@gmail.com>
2023-05-10 15:54:14 -0500

    Set up events controller, events index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,29 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current.user.hosted_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date, :start_time, :end_time, :details)
+  end
 end
